5734d8cf6eb9397e7295b4c4a6b11bf9e823796e
AlexSD <jesus2209.isc@gmail.com>
2025-09-15 17:56:15 -0600

    Cambio de prueba

--- SchoolData/Models/Class.cs
+++ SchoolData/Models/Class.cs
@@ -5,7 +5,6 @@ namespace SistemaEscolar.Models
     public class Class
     {
         public int IdClass { get; set; }
-
         [Required]
         [MaxLength(50)]
         public string ClassName { get; set; }
