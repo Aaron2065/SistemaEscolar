1214b3bd8bbe8c19be808e12816643533bda02c5
AlexSD <jesus2209.isc@gmail.com>
2025-09-15 23:55:42 -0600

    Se agrega UploadSettings y sus derivados

---
+++ UploadSettings/Services/Implementations/Tutor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolService.Services.Interfaces
+{
+    public interface Tutor : GenericService<TutorCreateDTO, TutorReadDTO, TutorCreateDTO>
+    {
+
+    }
+}
