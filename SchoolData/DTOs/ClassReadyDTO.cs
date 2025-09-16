d364c25d804d63cc7fea330c676ae776b60f27d0
AlexSD <jesus2209.isc@gmail.com>
2025-09-16 07:54:55 -0600

    Se crean DTOs

---
+++ SchoolData/DTOs/ClassReadyDTO.cs
@@ -0,0 +1,19 @@
+﻿using SistemaEscolar.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolData.DTOs
+{
+    public class ClassReadyDTO : RegistryDTO
+    {
+        public int IdClass { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string ClassName { get; set; }
+        public int IdCurse { get; set; }
+
+    }
+}
