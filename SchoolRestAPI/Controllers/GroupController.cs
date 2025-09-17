7f3b41ea4993b94031584f837d2cf6c687e25422
Aaron2065 <20001286@alumnos.utleon.edu.mx>
2025-09-16 23:20:06 -0600

    Realizar modulo Group

---
+++ SchoolRestAPI/Controllers/GroupController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolData.DTOs;
+using SchoolService.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace SchoolRestAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GroupsController : ControllerBase
+    {
+        private readonly IGroupService _groupService;
+
+        public GroupsController(IGroupService groupService)
+        {
+            _groupService = groupService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await _groupService.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _groupService.GetByIdAsync(id);
+            return result == null ? NotFound() : Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] GroupCreateDTO dto)
+        {
+            await _groupService.AddAsync(dto);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] GroupCreateDTO dto)
+        {
+            await _groupService.UpdateAsync(id, dto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _groupService.DeleteAsync(id);
+            return NoContent();
+        }
+
+        [HttpGet("WithTutorAndCourses")]
+        public async Task<IActionResult> GetGroupsWithTutorAndCourses()
+        {
+            var result = await _groupService.GetGroupsWithTutorAndCoursesAsync();
+            return Ok(result);
+        }
+    }
+}
--- UploadSettings/Services/Implementations/Group.cs
+++ UploadSettings/Services/Implementations/Group.cs
@@ -1,9 +1,102 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolData;
+using SchoolData.DTOs;
+using SchoolData.Models;
+using SchoolService.Services.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SchoolService.Services.Implementations
 {
+    public class GroupService : IGroupService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GroupService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<GroupReadDTO>> GetAllAsync()
+        {
+            return await _context.Groups
+                .Select(g => new GroupReadDTO
+                {
+                    IdGradeGroup = g.IdGradeGroup,
+                    Grade = g.Grade,
+                    GroupClass = g.GroupClass,
+                    IdTutor = g.IdTutor
+                })
+                .ToListAsync();
+        }
+
+        public async Task<GroupReadDTO> GetByIdAsync(int id)
+        {
+            var g = await _context.Groups.FindAsync(id);
+            if (g == null)
+                throw new KeyNotFoundException("Grupo no encontrado");
+
+            return new GroupReadDTO
+            {
+                IdGradeGroup = g.IdGradeGroup,
+                Grade = g.Grade,
+                GroupClass = g.GroupClass,
+                IdTutor = g.IdTutor
+            };
+        }
+
+        public async Task AddAsync(GroupCreateDTO dto)
+        {
+            var group = new Group
+            {
+                Grade = dto.Grade,
+                GroupClass = dto.GroupClass,
+                IdTutor = dto.IdTutor
+            };
+
+            await _context.Groups.AddAsync(group);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int id, GroupCreateDTO dto)
+        {
+            var group = await _context.Groups.FindAsync(id);
+            if (group == null)
+                throw new KeyNotFoundException("Grupo no encontrado");
+
+            group.Grade = dto.Grade;
+            group.GroupClass = dto.GroupClass;
+            group.IdTutor = dto.IdTutor;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var group = await _context.Groups.FindAsync(id);
+            if (group != null)
+            {
+                _context.Groups.Remove(group);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<GroupReadDTO>> GetGroupsWithTutorAndCoursesAsync()
+        {
+            return await _context.Groups
+                .Include(g => g.Tutor)
+                .Include(g => g.IdCourse)
+                .Select(g => new GroupReadDTO
+                {
+                    IdGradeGroup = g.IdGradeGroup,
+                    Grade = g.Grade,
+                    GroupClass = g.GroupClass,
+                    IdTutor = g.IdTutor,
+                    TutorName = g.Tutor.Employee.Name,
+                    IdCourses = g.IdCourse.Select(c => c.IdCourse).ToList()
+                })
+                .ToListAsync();
+        }
+    }
 }
